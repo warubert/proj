6b1085520f7fd1d6c5bc49998da0073e6803052e
warubert <william.rubert@gmail.com>
2026-04-07 19:38:01 -0300

    fix: ajusta variavel do node

--- infra/email.js
+++ infra/email.js
@@ -8,7 +8,7 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_SMTP_USER,
     pass: process.env.EMAIL_SMTP_PASSWORD,
   },
-  secure: process.env.ENV_NODE === "production" ? true : false,
+  secure: process.env.NODE_ENV === "production" ? true : false,
 });
 
 async function send(mailOptions) {
