bcda50bd9734e6b86d0bb539a56babf473f6ab2f
warubert <william.rubert@gmail.com>
2026-06-30 08:36:29 -0300

    feat: useState

--- pages/cadastro/index.js
+++ pages/cadastro/index.js
@@ -1,19 +1,32 @@
+import { useState } from "react";
+
 export default function RegisterPage() {
   console.log("RegisterPage rendered");
 
+  const [newCount, setNewCount] = useState(0);
+
   let count = 0;
   console.log("Count:", count);
+  console.log("newCount:", newCount);
 
   function incrementCount() {
     count++;
     console.log("Count incremented:", count);
   }
 
+  function incrementNewCount() {
+    setNewCount(newCount + 1);
+    console.log("newCount incremented:", newCount + 1);
+  }
+
   return (
     <>
       <h1>Count: {count}</h1>
       <button onClick={incrementCount}>Increment count</button>
+      <h1>New Count: {newCount}</h1>
+      <button onClick={incrementNewCount}>Increment new count</button>
       <h1>Cadastro</h1>
+      <hr />
       <ComponenteA />
       <ComponenteB />
     </>
